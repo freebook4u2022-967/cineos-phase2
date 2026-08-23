589d726eb13356136b6d023814fa715e7a35ccd1
freebook4u2022-967 <freebook4u2022@gmail.com>
2026-08-24 00:00:29 +0800

    Export temporal identity memory and QC API

--- src/cineos/native_image/__init__.py
+++ src/cineos/native_image/__init__.py
@@ -6,11 +6,25 @@ from .conditioning import (
     NativeImageConditioningPlan,
     compile_native_image_plan,
 )
+from .temporal_identity import (
+    IdentityObservation,
+    IdentityObservationError,
+    IdentityQCReport,
+    IdentityVisualQCGate,
+    TemporalIdentityMemory,
+    apply_temporal_identity_memory,
+)
 
 __all__ = [
     "NATIVE_IMAGE_PLAN_SCHEMA",
+    "IdentityObservation",
+    "IdentityObservationError",
+    "IdentityQCReport",
+    "IdentityVisualQCGate",
     "NativeImageConditioningPlan",
     "NativeImageResearchBackend",
     "NativeImageResearchResult",
+    "TemporalIdentityMemory",
+    "apply_temporal_identity_memory",
     "compile_native_image_plan",
 ]
