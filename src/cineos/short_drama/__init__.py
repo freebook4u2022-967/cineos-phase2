bfcf74aae55d24a89b1a3560e76cb6a293846c5d
freebook4u2022-967 <freebook4u2022@gmail.com>
2026-08-23 22:29:13 +0800

    Sprint 2: export creative brain APIs

--- src/cineos/short_drama/__init__.py
+++ src/cineos/short_drama/__init__.py
@@ -1,22 +1,23 @@
 """CINEOS Short Drama Agent orchestration primitives."""
 
-from .agents import (
-    ContinuitySupervisor,
-    DirectorAgent,
-    ScreenwriterAgent,
-    ShotPlanner,
-    StoryArchitect,
-)
-from .models import DramaBrief, DramaPlan
+from .agents import ContinuitySupervisor, ScreenwriterAgent, ShotPlanner
+from .brains import CharacterBrain, DramaBrain
+from .directing import DirectorDecisionEngine
+from .models import CharacterProfile, DramaBrief, DramaPlan, SceneState
 from .orchestrator import ShortDramaOrchestrator
+from .state import SceneStateEngine
 
 __all__ = [
+    "CharacterBrain",
+    "CharacterProfile",
     "ContinuitySupervisor",
-    "DirectorAgent",
+    "DirectorDecisionEngine",
+    "DramaBrain",
     "DramaBrief",
     "DramaPlan",
+    "SceneState",
+    "SceneStateEngine",
     "ScreenwriterAgent",
     "ShortDramaOrchestrator",
     "ShotPlanner",
-    "StoryArchitect",
 ]
