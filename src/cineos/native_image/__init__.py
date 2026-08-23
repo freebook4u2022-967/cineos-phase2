41b34d001366229fb2d65c43f274fe1980ce7655
freebook4u2022-967 <freebook4u2022@gmail.com>
2026-08-24 00:06:51 +0800

    Export Sprint 6 visual QC API

--- src/cineos/native_image/__init__.py
+++ src/cineos/native_image/__init__.py
@@ -14,17 +14,31 @@ from .temporal_identity import (
     TemporalIdentityMemory,
     apply_temporal_identity_memory,
 )
+from .visual_qc import (
+    VISUAL_QC_AXES,
+    MultiAxisVisualQCGate,
+    VisualContinuityMemory,
+    VisualContinuityObservation,
+    VisualQCReport,
+    build_rerender_directives,
+)
 
 __all__ = [
     "NATIVE_IMAGE_PLAN_SCHEMA",
+    "VISUAL_QC_AXES",
     "IdentityObservation",
     "IdentityObservationError",
     "IdentityQCReport",
     "IdentityVisualQCGate",
+    "MultiAxisVisualQCGate",
     "NativeImageConditioningPlan",
     "NativeImageResearchBackend",
     "NativeImageResearchResult",
     "TemporalIdentityMemory",
+    "VisualContinuityMemory",
+    "VisualContinuityObservation",
+    "VisualQCReport",
     "apply_temporal_identity_memory",
+    "build_rerender_directives",
     "compile_native_image_plan",
 ]
