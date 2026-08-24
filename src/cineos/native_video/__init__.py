1d01bc98a901ca7e058d0e772c50dc8679bef778
freebook4u2022-967 <freebook4u2022@gmail.com>
2026-08-24 14:34:44 +0800

    Add native video package foundation

---
+++ src/cineos/native_video/__init__.py
@@ -0,0 +1,15 @@
+"""CINEOS-owned native video temporal modeling contracts."""
+
+from .temporal_model import (
+    NativeTemporalModel,
+    TemporalFrameInput,
+    TemporalFrameOutput,
+    TemporalSequenceState,
+)
+
+__all__ = [
+    "NativeTemporalModel",
+    "TemporalFrameInput",
+    "TemporalFrameOutput",
+    "TemporalSequenceState",
+]
